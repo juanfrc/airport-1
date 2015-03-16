ae6813f5f96c046741ca7f2bd5facc8ac99af16e
juanfrc <juanfrc2@gmail.com>
2015-03-16 10:00:40 -0500

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,9 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+ # Mayor.create(name: 'Emanuel', city: cities.first)
+
 ["Avianca", "Chiva Colombia"].each do |airline|
 	Airline.create(name: airline)
 end
@@ -13,17 +14,19 @@ end
 @aerolinea2 = Airline.last.id
 
 Flight.create(number: 21, airline_id: @aerolinea1)
-Departue.create(flight_id: 1, date: Time.now, status:"On gate")
+Departure.create(flight_id: 1, date: Time.now, status:"On gate")
 Arrival.create(flight_id:1, date: Time.now + 5.hours, status:"On time")
+
 Flight.create(number: 24, airline_id: @aerolinea1)
-Departue.create(flight_id: 2, date: Time.now + 12.minutes, status:"Closed")
+Departure.create(flight_id: 2, date: Time.now + 12.minutes, status:"Closed")
 Arrival.create(flight_id:2, date: Time.now + 45.minutes, status:"Delayed")
 
 Flight.create(number: 543, airline_id: @aerolinea2)
-Departue.create(flight_id: 3, date: Time.now + 23.minutes, status:"Boarding")
+Departure.create(flight_id: 3, date: Time.now + 23.minutes, status:"Boarding")
 Arrival.create(flight_id:3, date: Time.now + 10.hours, status:"Landing")
+
 Flight.create(number: 542, airline_id: @aerolinea2)
-Departue.create(flight_id: 4, date: Time.now - 15.minutes, status:"Took off")
+Departure.create(flight_id: 4, date: Time.now - 15.minutes, status:"Took off")
 Arrival.create(flight_id:4, date: Time.now + 90.minutes, status:"Landed")
 
 
